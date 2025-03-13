62f191fdf5b0f02f36f00073cd4c33aa6f7dcffc
Levian Amos <148203267+Mozelkrypton@users.noreply.github.com>
2025-03-13 03:10:33 +0300

    Add files via upload

---
+++ mid1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    printf("time it up");
+
+    int red=4;
+    printf("%d",red);
+    // a for loop used to execute a block of code a number of times 
+    // for(initialization, condition, de/incrementation)
+    for(int i=red; i<=4; i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}
+
