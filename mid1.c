4edd345993378b329c21282826e1fed41ab03494
Levian Amos <148203267+Mozelkrypton@users.noreply.github.com>
2025-03-14 09:33:09 +0300

    Update mid1.c

--- mid1.c
+++ mid1.c
@@ -3,11 +3,13 @@
 int main(){
     printf("time it up");
 
-    int red=4;
+    int red=1;
     printf("%d",red);
     // a for loop used to execute a block of code a number of times 
+    //this for loop lists all odd numbers from 1 to 24 exept 3
     // for(initialization, condition, de/incrementation)
-    for(int i=red; i<=4; i--){
+    for(int i=red; i<=24; i+=2){
+        if(i==3)continue;
         printf("%d\n",i);
     }
     return 0;
